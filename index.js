6a145c2c45bfe14c723e676174cd37a6a9d7c54e
FFarquar <110162452+FFarquar@users.noreply.github.com>
2023-04-18 13:15:02 +1000

    Big changes. No variable

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ app.use(express.static("public"));
 
 const db = async () => {
   try {
-    console.log("In db conn before setting connection. Listname = " + listName);
+    //console.log("In db conn before setting connection. Listname = " + listName);
+    //live site
     const conn = await mongoose.connect('mongodb+srv://DD_1:DD_1_PW@cluster1.eiy6kz9.mongodb.net/test');
-    console.log("In db conn after setting connection. Listname = " + listName);
+
+    //loocal site
+    //const conn = await mongoose.connect('mongodb://127.0.0.1/test');
+    //console.log("In db conn after setting connection. Listname = " + listName);
     console.log("MonoDB Connected: " + conn.connection.host);
       
     } catch (error) {
@@ -30,7 +34,7 @@ const db = async () => {
 }
 
 //let listName = "All items";
-let listName;
+//let listName;
 
 
 const itemsSchema = new mongoose.Schema({
@@ -48,7 +52,30 @@ app.get("/", async function(req, res) {
   console.log("In home get");
   const lists = await Item_m.distinct("list", {"deleted":"false"});
   //want to have the first list selected if 
-  console.log(" listName variable = " + listName);
+  //console.log(" listName variable = " + listName);
+
+  //if the home route is triggered, return the first list in the collection
+
+  //console.log("Listname is blank");
+  //find the first and select it
+  if (lists.length > 0) {
+    //there is a list to select from
+    console.log("   Initial list name is  " + lists[0]);
+    //listName = lists[0];
+
+    const items = await Item_m.find({list:lists[0], deleted:false});
+  
+    res.render("simple_list_test", {listTitle: lists[0], newListItems: items, uniqueLists:lists});
+  
+
+  } else {
+    //if there are no lists, go to the create new list page
+    console.log("No lists found");
+     //note: this hasnt been tested as database has records.
+    res.render("newList", {listWarning: undefined, item: undefined});
+  }
+/* 
+
   if (listName == undefined) {
     console.log("Listname is blank");
     //find the first and select it
@@ -76,24 +103,29 @@ app.get("/", async function(req, res) {
   
     res.render("simple_list_test", {listTitle: listName, newListItems: items, uniqueLists:lists});
   }
-
+ */
 });
 
 app.post("/", async function(req, res){
 
   const item = req.body.newItem;
+  const listTitle = req.body.list;
 
-  console.log("Posting new item.  Item text = " + item);
-  console.log("Posting new item.  List name = " + listName);
+  console.log("___________________");
+  console.log("Posting new item");
+  console.log("   Item text = " + item);
+  console.log("   List name = " + listTitle);
+  console.log("___________________");
 
+  const lists = await Item_m.distinct("list", {"deleted":"false"});
+  
   //listName is a stored variable
-  const newItem = await Item_m.create({ list: listName, name: item, "$set":{"checked":false}, "$set":{"deleted":false}})
-  .then(function (newItem) {
-    res.redirect("/");
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+  const newItem = await Item_m.create({ list: listTitle, name: item, "$set":{"checked":false}, "$set":{"deleted":false}});
+  const items = await Item_m.find({list:listTitle, deleted:false});
+
+  res.render("simple_list_test", {listTitle: listTitle, newListItems: items, uniqueLists:lists});
+
+//    res.redirect("/");
   
 
 });
@@ -132,7 +164,15 @@ app.post("/addnewList", async function (req, res)  {
       res.render("newList", {listWarning:listNewName, item:newItem1});
     }  else {
 
-      const newItem = await Item_m.create({ list: listNewName, name: newItem1, "$set":{"checked":false}, "$set":{"deleted":false}})
+      const newItem = await Item_m.create({ list: listNewName, name: newItem1, "$set":{"checked":false}, "$set":{"deleted":false}});
+
+      const items = await Item_m.find({list:listNewName, deleted:false});
+
+      const lists = await Item_m.distinct("list", {"deleted":"false"});
+    
+      res.render("simple_list_test", {listTitle: listNewName, newListItems: items, uniqueLists:lists});
+
+/*       const newItem = await Item_m.create({ list: listNewName, name: newItem1, "$set":{"checked":false}, "$set":{"deleted":false}})
       .then(function (newItem) {
 
         listName = listNewName;
@@ -141,7 +181,7 @@ app.post("/addnewList", async function (req, res)  {
       .catch(function (err) {
         console.log(err);
       });
-    }
+ */    }
   } 
 });
 
@@ -152,23 +192,107 @@ app.get("/list/:list", async function (req, res)  {
   console.log("   req.params.list = "  + req.params.list);
 
   listName = req.params.list;
-  //console.log("Different list requested  app.get(/list/:list"  + listName);
   console.log("listName variable  =  " + listName);
 
-//   const items = await Item_m.find({list:listName, deleted:false});
-        
-// //  console.log("List name not blank it =" + listName);
-//   const lists = await Item_m.distinct("list", {"deleted":"false"});
+  const items = await Item_m.find({list:listName, deleted:false});
 
-//   console.log("Rendering list directly from list/:list");
+  const lists = await Item_m.distinct("list", {"deleted":"false"});
+
+  console.log("Rendering list directly from list/:list");
   
-//   res.render("simple_list_test", {listTitle: listName, newListItems: items, uniqueLists:lists});
-
-  res.redirect("/");
+  res.render("simple_list_test", {listTitle: listName, newListItems: items, uniqueLists:lists});
 
 });
 
 //app.get("/itemChangeCheckedStatus/:item", function (req, res)  {
+
+
+
+app.post("/itemChange", async function (req, res)  {
+
+  console.log("*************");
+  console.log("  In item change");
+
+  let checkBoxID = req.body.changeCheckedStatus;
+
+//  let deleteButtonID = req.body.deleteButton;
+
+  console.log("Checkbox ID = " + checkBoxID);
+
+  const newItemupdateItem = await Item_m.updateOne(
+    { _id: checkBoxID },
+    [ { "$set": { "checked": { "$eq": [false, "$checked"] } } } ]
+    )
+    //console.log("Count of updated records = " + newItemupdateItem.modifiedCount);
+
+  const listTitle = req.body.listName;
+  console.log("  List title = " + listTitle);
+  const items = await Item_m.find({list:listTitle, deleted:false});
+        
+  const lists = await Item_m.distinct("list", {"deleted":"false"});
+    
+  res.render("simple_list_test", {listTitle: listTitle, newListItems: items, uniqueLists:lists});
+
+
+//  res.redirect("/")
+
+  
+
+/*   const url = require('url');
+  const querystring = require('querystring');
+
+  let parsedUrl = url.parse(req.url);
+  let parsedQs = querystring.parse(parsedUrl.query);
+
+  let idOfItem = parsedQs.checkbox
+
+  // have the id of the checkbox now can read the value and flip it
+
+  //console.log("parsedQs " + parsedQs);
+  res.send("idOfItem = " + idOfItem); */
+
+
+});
+
+app.post("/deleteItem/:item", async function (req, res)  {
+  console.log("*************");
+  console.log("  Item delete");
+  let deleteButtonID = req.body.deleteButton;
+  const listTitle = req.body.listName;
+  console.log("  List title = " + listTitle);
+
+  console.log("   Delete button = " + deleteButtonID);
+  //mark the item as deleted
+  const itemupdateItem = await Item_m.updateOne({ _id: deleteButtonID},  {"$set":{"deleted":true}})
+  
+  
+
+
+  const items = await Item_m.find({list:listTitle, deleted:false});
+        
+  const lists = await Item_m.distinct("list", {"deleted":"false"});
+    
+  res.render("simple_list_test", {listTitle: listTitle, newListItems: items, uniqueLists:lists});
+});
+
+
+
+
+/*  db().then(() => {
+    console.log("In app listen before app.listen Listname = " + listName);
+     app.listen(process.env.PORT || 3000, function(){
+      console.log("In app listen after app.listen Listname = " + listName);
+       console.log("server is running on port "+ PORT)
+     })
+ }); */
+
+
+db().then(() => {
+  app.listen(PORT, ()=> {
+    console.log("Listening for requests");
+  })
+});
+
 
 //this works but it should be a post method because data is changing
 /*   app.get("/itemChangeCheckedStatus", function (req, res)  {
@@ -189,70 +313,3 @@ app.get("/list/:list", async function (req, res)  {
     //console.log("parsedQs " + parsedQs);
     res.send("idOfItem = " + idOfItem);
 }); */
-
-
-app.post("/itemChange", async function (req, res)  {
-
-  let checkBoxID = req.body.changeCheckedStatus;
-
-  let deleteButtonID = req.body.deleteButton;
-
-/*   console.log("Checkbox ID = " + checkBoxID);
-  console.log("deleteButtonID = " + deleteButtonID); */
-
-
-  if (checkBoxID != undefined) {
-    //The checkbox to change the checked status has been selected
-
-   // const newItemupdateItem = await Item_m.updateOne({_id: checkBoxID, checked: "false"})
-
-    const newItemupdateItem = await Item_m.updateOne(
-      { _id: checkBoxID },
-      [ { "$set": { "checked": { "$eq": [false, "$checked"] } } } ]
-      )
-    //console.log("Count of updated records = " + newItemupdateItem.modifiedCount);
-  } else {
-    //the only other thing to do on the form is to delete an item
-    if (deleteButtonID != undefined) {
-      const itemupdateItem = await Item_m.updateOne(
-        { _id: deleteButtonID},  {"$set":{"deleted":true}})
-     
-    }
-  }
-  res.redirect("/")
-
-  
-
-/*   const url = require('url');
-  const querystring = require('querystring');
-
-  let parsedUrl = url.parse(req.url);
-  let parsedQs = querystring.parse(parsedUrl.query);
-
-  let idOfItem = parsedQs.checkbox
-
-  // have the id of the checkbox now can read the value and flip it
-
-  //console.log("parsedQs " + parsedQs);
-  res.send("idOfItem = " + idOfItem); */
-
-
-});
-
-
-
-
-/*  db().then(() => {
-    console.log("In app listen before app.listen Listname = " + listName);
-     app.listen(process.env.PORT || 3000, function(){
-      console.log("In app listen after app.listen Listname = " + listName);
-       console.log("server is running on port "+ PORT)
-     })
- }); */
-
-
-db().then(() => {
-  app.listen(PORT, ()=> {
-    console.log("Listening for requests");
-  })
-});
